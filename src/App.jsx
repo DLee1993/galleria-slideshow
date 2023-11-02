03a7785268bc6a37dc2884895205ea8d68f66f50
David Lee <Dai180293@gmail.com>
2023-11-02 03:56:19 +0000

    added slideshow

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,63 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import logo from "./assets/shared/logo.svg";
 import galleryData from "./assets/dataset/data.json";
+import { Fade } from "react-slideshow-image";
+import prev from "./assets/shared/icon-back-button.svg";
+import next from "./assets/shared/icon-next-button.svg";
+import "react-slideshow-image/dist/styles.css";
 const App = () => {
+    const slideRef = useRef(null);
     const [slideshow, setSlideShow] = useState(false);
+    const [autoplaySlideshow, setAutoplaySlideshow] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
+    useEffect(() => {
+        slideRef.current.goTo(parseInt(selectedIndex));
+    }, [selectedIndex]);
+
+    useEffect(() => {
+        if (slideshow) {
+            setAutoplaySlideshow(true);
+        } else {
+            setAutoplaySlideshow(false);
+        }
+    }, [slideshow]);
+
+    const prevStyle = {
+        position: "absolute",
+        bottom: "25px",
+        left: "auto",
+        right: "50px",
+        width: "30px",
+        height: "30px",
+        background: "none",
+        border: "0px",
+    };
+    const nextStyle = {
+        position: "absolute",
+        bottom: "25px",
+        right: "0px",
+        width: "30px",
+        height: "30px",
+        background: "none",
+        border: "0px",
+    };
+
+    const properties = {
+        prevArrow: (
+            <button style={{ ...prevStyle }}>
+                <img src={prev} alt="previous slide" aria-label="Previous Slide" data-type="prev" />
+            </button>
+        ),
+        nextArrow: (
+            <button style={{ ...nextStyle }}>
+                <img src={next} alt="next slide" aria-label="Next Slide" data-type="next" />
+            </button>
+        ),
+    };
+
     return (
-        <>
+        <main>
             <header className="max-w-[calc(100%-3rem)] mx-auto flex justify-between items-center h-20 border-b-2 border-silver">
                 <img src={logo} alt="website name" />
                 <button
@@ -14,13 +67,16 @@ const App = () => {
                     {slideshow ? "stop slideshow" : "start slideshow"}
                 </button>
             </header>
-            <main>
-                <section id="art_grid_container" className="max-w-[calc(100%-3rem)] mx-auto my-5">
+            <section className="max-w-[calc(100%-3rem)] mx-auto my-5">
+                <section id="art_grid_container" className={!slideshow ? "block" : "hidden"}>
                     {galleryData.map((article, index) => (
                         <article
                             key={index}
                             className="relative mb-5 hover:cursor-pointer hover:opacity-75 hover:scale-105 scale-100 transition-all"
-                            onClick={() => setSlideShow(!slideshow)}
+                            onClick={() => {
+                                setSlideShow(!slideshow);
+                                setSelectedIndex(index);
+                            }}
                         >
                             <figure className="bg-black">
                                 <img
@@ -36,8 +92,28 @@ const App = () => {
                         </article>
                     ))}
                 </section>
-            </main>
-        </>
+                <section className={`${slideshow ? "block" : "hidden"}`}>
+                    <Fade
+                        defaultIndex={selectedIndex}
+                        autoplay={autoplaySlideshow}
+                        ref={slideRef}
+                        canSwipe={true}
+                        prevArrow={properties.prevArrow}
+                        nextArrow={properties.nextArrow}
+                    >
+                        {galleryData.map((slide, index) => (
+                            <section key={index}>
+                                <img src={slide.images.gallery} alt="" />
+                                <aside className="flex justify-center items-start flex-col h-20 mt-5 border-t-2 border-silver">
+                                    <p>{slide.name}</p>
+                                    <p>{slide.artist.name}</p>
+                                </aside>
+                            </section>
+                        ))}
+                    </Fade>
+                </section>
+            </section>
+        </main>
     );
 };
 
