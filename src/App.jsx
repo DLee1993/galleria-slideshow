eed5cc3631f712f9de2675bf4cba19b4fd8e820e
David Lee <Dai180293@gmail.com>
2023-11-08 14:07:11 +0000

    removed the react-slideshow-image components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useEffect, useRef, useState } from "react";
 import logo from "./assets/shared/logo.svg";
 import galleryData from "./assets/dataset/data.json";
-import { Fade } from "react-slideshow-image";
-import prev from "./assets/shared/icon-back-button.svg";
-import next from "./assets/shared/icon-next-button.svg";
-import expand from "./assets/shared/icon-view-image.svg";
-import "react-slideshow-image/dist/styles.css";
+// import prev from "./assets/shared/icon-back-button.svg";
+// import next from "./assets/shared/icon-next-button.svg";
+// import expand from "./assets/shared/icon-view-image.svg";
+
 const App = () => {
     const slideRef = useRef(null);
     const [slideshow, setSlideShow] = useState(false);
@@ -15,49 +14,12 @@ const App = () => {
     const [galleryImage, setGalleryImage] = useState("");
 
     useEffect(() => {
-        slideRef.current.goTo(parseInt(selectedIndex));
-    }, [selectedIndex]);
-
-    useEffect(() => {
         if (slideshow) {
             setAutoplaySlideshow(true);
         } else {
             setAutoplaySlideshow(false);
         }
     }, [slideshow]);
-
-    const prevStyle = {
-        position: "absolute",
-        bottom: "20px",
-        left: "auto",
-        right: "50px",
-        width: "30px",
-        height: "30px",
-        background: "none",
-        border: "0px",
-    };
-    const nextStyle = {
-        position: "absolute",
-        bottom: "20px",
-        right: "0px",
-        width: "30px",
-        height: "30px",
-        background: "none",
-        border: "0px",
-    };
-
-    const properties = {
-        prevArrow: (
-            <button style={{ ...prevStyle }} data-type="prev">
-                <img src={prev} alt="previous slide" aria-label="Previous Slide" />
-            </button>
-        ),
-        nextArrow: (
-            <button style={{ ...nextStyle }} data-type="next">
-                <img src={next} alt="next slide" aria-label="Next Slide" />
-            </button>
-        ),
-    };
 
     const toggleGallery = (e) => {
         if (e) {
@@ -121,75 +83,13 @@ const App = () => {
                     ))}
                 </section>
                 <section className={`${slideshow ? "block" : "hidden"}`}>
-                    <Fade
-                        defaultIndex={selectedIndex}
-                        ref={slideRef}
-                        autoplay={false}
-                        transitionDuration={500}
-                        canSwipe={true}
-                        pauseOnHover={false}
-                        prevArrow={properties.prevArrow}
-                        nextArrow={properties.nextArrow}
-                    >
+                    {/* Add swiper container here */}
+                    <section>
+                        {/* loop through and add a swiperSlide per slide */}
                         {galleryData.map((slide, index) => (
-                            <section key={index}>
-                                <section className="flex flex-wrap justify-evenly items-center h-screen">
-                                    <figure className="relative w-full max-w-[475px]">
-                                        <picture>
-                                            <source
-                                                media="(min-width: 900px)"
-                                                srcSet={slide.images.hero.large}
-                                            />
-                                            <source
-                                                media="(min-width: 650px)"
-                                                srcSet={slide.images.hero.small}
-                                            />
-                                            <source
-                                                media="(max-width: 649px)"
-                                                srcSet={slide.images.thumbnail}
-                                            />
-                                            <img
-                                                src={slide.images.hero.large}
-                                                alt="gallery"
-                                                className="w-full"
-                                            ></img>
-                                        </picture>
-                                        <button
-                                            onClick={() => toggleGallery(slide.images.hero.small)}
-                                            className="flex justify-evenly items-center absolute top-5 sm:top-auto sm:bottom-5 left-5 text-white bg-black hover:bg-black/50 transition-colors w-36 h-10 uppercase text-xs tracking-widest"
-                                        >
-                                            <span>
-                                                <img src={expand} alt="view full image" />
-                                            </span>
-                                            view image
-                                        </button>
-                                    </figure>
-                                    <section className="relative">
-                                        <span className="absolute top-10 right-0 text-8xl md:text-[200px] text-silver -z-10">
-                                            {slide.year}
-                                        </span>
-                                        <section>
-                                            <article className="text-grey font-bold leading-7 text-sm md:max-w-[350px]">
-                                                {slide.description}
-                                            </article>
-                                            <a
-                                                href={slide.source}
-                                                target="_blank"
-                                                rel="noreferrer"
-                                                className="inline-block text-[9px] mt-10 font-bold uppercase underline text-grey hover:text-black transition-colors tracking-widest"
-                                            >
-                                                go to source
-                                            </a>
-                                        </section>
-                                    </section>
-                                </section>
-                                <aside className="flex justify-center items-start flex-col mt-5 h-20 border-t-2 border-silver">
-                                    <p>{slide.name}</p>
-                                    <p>{slide.artist.name}</p>
-                                </aside>
-                            </section>
+                            <section key={index}>{slide.name}</section>
                         ))}
-                    </Fade>
+                    </section>
                 </section>
             </section>
         </main>
